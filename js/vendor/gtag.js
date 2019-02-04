215bc98ba1c1f41a2154408c634e306b053c4590
Sean Vreeland <seanvree@hotmail.com>
2019-02-03 17:50:53 -0700

    testing in progress

--- js/vendor/gtag.js
+++
@@ -1,9 +0,0 @@
-// <!-- Global site tag (gtag.js) - Google Analytics -->
-src="https://www.googletagmanager.com/gtag/js?id=UA-133756821-1";
-
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-133756821-1');
-
